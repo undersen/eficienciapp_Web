34b0243d417bdf644a4255c086176cf50105a44e
undersen <under.sen@live.cl>
2015-09-08 14:28:46 -0300

    se añade daemon de automatix¡zacion para sass y para js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
+  sass = require('gulp-sass'),
 	minifyHTML = require('gulp-minify-html');
 
 
-gulp.task('minify-html', function() {
+gulp.task('html', function() {
   var opts = {
     conditionals: true,
     spare:true,
@@ -18,5 +19,16 @@ gulp.task('minify-html', function() {
 gulp.task('compress', function() {
   return gulp.src('lib/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('sass', function () {
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.scss', ['sass','compress']);
 });
