7ac05f6d87fee2f8885ba22d11c4a9685a3ebadf
undersen <under.sen@live.cl>
2015-09-14 11:41:56 -0300

    workspace

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-  sass = require('gulp-sass'),
-	minifyHTML = require('gulp-minify-html');
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    image = require('gulp-image'),
+    minifyHTML = require('gulp-minify-html'),
+    imagemin = require('gulp-imagemin'),
+    imageResize = require('gulp-image-resize'),
+    pngquant = require('imagemin-pngquant');
+
+var paths = {
+  sass: ['./www/css/*.scss'],
+  js: ['./www/js/Main.js', './www/js/Config.js', './www/js/Run.js', './www/js/*/*.js']
+};
+
+gulp.task('resize', function () {
+  gulp.src('start.png')
+    .pipe(imageResize({ 
+      width:0,
+      height : 80,
+      crop : true,
+      upscale : true
+    }))
+    .pipe(gulp.dest('dist'));
+});
 
 
 gulp.task('html', function() {
@@ -9,26 +29,41 @@ gulp.task('html', function() {
     conditionals: true,
     spare:true,
     comments:true  };
- 
-  return gulp.src('src/html/*.html')
+
+    return gulp.src('src/html/*.html')
     .pipe(minifyHTML(opts))
     .pipe(gulp.dest('dist/'));
+  });
+
+gulp.task('image', function () {
+  gulp.src('./src/media/images/iphone/*.jpg')
+    .pipe(image())
+    .pipe(gulp.dest('./dist/media/'));
+});
+ 
+ gulp.task('imagemin', function () {
+    return gulp.src('./dist/media/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
 });
 
-
 gulp.task('compress', function() {
-  return gulp.src('lib/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+  return gulp.src('./src/lib/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('./'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('src/sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+  return gulp.src('./src/sass/*.scss')
+     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css/'))
 });
- 
+
 gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.scss', ['sass','compress']);
 });
